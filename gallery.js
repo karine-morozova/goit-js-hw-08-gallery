de49b6328c6d42f12cf538fd3d541e2a81515e05
karine-morozova <morozova_karine@ukr.net>
2020-09-27 15:47:40 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,79 +1,25 @@
-const images = [
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-      description: 'Hokkaido Flower',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-      description: 'Container Haulage Freight',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-      description: 'Aerial Beach View',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-      description: 'Flower Blooms',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-      description: 'Alpine Mountains',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-      description: 'Mountain Lake Sailing',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-      description: 'Alpine Spring Meadows',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-      description: 'Nature Landscape',
-    },
-    {
-      preview:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-      original:
-        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-      description: 'Lighthouse Coast Sea',
-    },
-  ];
+import galleryItems from "./gallery-items.js"
 
   const galleryEl = document.querySelector(".js-gallery");
-  console.log(galleryEl);
+  const modalEl = document.querySelector(".js-lightbox");
+  // const galleryItemEl = document.querySelector(".gallery__item");
+const overlayEl = document.querySelector(".lightbox__overlay");
+// const modalContentEl = document.querySelector(".lightbox__content");
+const modalImgEl = document.querySelector(".lightbox__image");
+const closeBtnEl = document.querySelector(".lightbox__button");
 
-  const galleryListEl = createGalleryList(images);
-  console.log(galleryListEl);
+   const galleryListEl = createGalleryList(galleryItems);
 
    galleryEl.insertAdjacentHTML("beforeend",galleryListEl);
 
-  function createGalleryList ({original, description, preview}) {
-    return  images.map(image => {`<li class="gallery__item">
+  
+   galleryEl.addEventListener("click", openModal);
+   closeBtnEl.addEventListener("click", closeModal);
+   overlayEl.addEventListener("click", closeModalOnOverlay);
+
+  function createGalleryList(img) {
+    return  img.map(({original, description, preview}) => {
+      return `<li class="gallery__item">
     <a
       class="gallery__link"
       href="${original}"
@@ -86,14 +32,30 @@ const images = [
       />
     </a>
   </li>`}).join("");
-  }
+  }  
 
  function openModal (event) {
+   event.preventDefault();
    if(event.target.nodeName !== "IMG") { 
      return
     };
+    
+    modalEl.classList.add("is-open");
+    modalImgEl.src = event.currentTarget.src;
+    modalImgEl.alt = event.currentTarget.alt;
 
+ }
 
+ function closeModal (event) {
+  modalEl.classList.remove("is-open");
+  modalImgEl.src = "";
+    
+
+ }
+ function closeModalOnOverlay (event) {
+   if (event.currentTarget === event.target) {
+    modalEl.classList.remove("is-open");
+   }
  }
 
  
